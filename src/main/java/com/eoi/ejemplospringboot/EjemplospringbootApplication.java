aac5d44d9a7b16b86108646ce361c754be41d568
Diego Ortega <diegoortegafp@gmail.com>
2023-05-24 18:06:10 +0200

    Initialize readme

---
+++ src/main/java/com/eoi/ejemplospringboot/EjemplospringbootApplication.java
@@ -0,0 +1,15 @@
+package com.eoi.ejemplospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class EjemplospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplospringbootApplication.class, args);
+	}
+
+}
